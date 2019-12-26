e6ce2f5536a05ee3964decb441bf12adffdd1f37
Sergey Pushkovskiy <onyxpsi@ukr.net>
2019-12-26 09:24:51 +0200

    Updates

---
+++ precache-manifest.1a29b6e2bce74848bf8edcaf98a8522b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd87b06671325854f1d6692c51a94a3",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "b72d98e5afbde0bca8e6",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5ff4418d.chunk.css"
+  },
+  {
+    "revision": "05b9ca301179d4937fdc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.68719948.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.68719948.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b72d98e5afbde0bca8e6",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.db89761a.chunk.js"
+  },
+  {
+    "revision": "9719fa00745b8fafea01",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.0c92875d.js"
+  }
+]);
